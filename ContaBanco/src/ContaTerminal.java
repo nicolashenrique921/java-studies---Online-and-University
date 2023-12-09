ebeb85a66527294bcaf1e53b9607e6207e8efe32
Nicolas <nicolashenrique921@gmail.com>
2023-12-09 15:05:27 -0300

    Simulando Uma Conta Bancária Através Do Terminal/Console

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,8 +1,10 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
 
         int numConta;
         String agencia;
