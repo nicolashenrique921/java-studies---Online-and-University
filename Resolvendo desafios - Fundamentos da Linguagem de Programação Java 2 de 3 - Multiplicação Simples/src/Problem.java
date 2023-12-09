e6a7453cf3b915c888bffd20c97bda88db740c1c
Nicolas <nicolashenrique921@gmail.com>
2023-12-09 04:23:50 -0300

    Resolucao desafio 2 de 3

---
+++ Resolvendo desafios - Fundamentos da Linguagem de Programação Java 2 de 3 - Multiplicação Simples/src/Problem.java
@@ -0,0 +1,39 @@
+/*  Abaixo segue um exemplo de código que você pode ou não utilizar
+import java.util.Scanner;
+
+public class Problem {
+
+	public static void main(String[] args) {
+	 	Scanner sc = new Scanner (System.in);
+ 
+		int A, B, soma;
+ 
+ 		A = sc.nextInt();
+		B = sc.nextInt();
+ //TODO: Complete os espaços em branco com uma possível solução para o desafio 
+ 		soma =  ;  
+ 
+ 		System.out.println("SOMA = " +        );
+	}
+} 
+*/
+
+// Abaixo o codigo ja resolvido por mim
+
+import java.util.Scanner;
+
+public class Problem {
+
+	public static void main(String[] args) {
+	 	Scanner sc = new Scanner (System.in);
+ 
+		int A, B, soma;
+ 
+ 		A = sc.nextInt();
+		B = sc.nextInt();
+ //TODO: Complete os espaços em branco com uma possível solução para o desafio 
+ 		soma = A+B ;  
+ 
+ 		System.out.println("SOMA = " +  soma      );
+	}
+}
