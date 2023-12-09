d47192e314292b569e48f4d4dc9a92e6e8521e2d
Nicolas <nicolashenrique921@gmail.com>
2023-12-09 04:31:49 -0300

    Resultado do desafio 3 de 3

---
+++ Resolvendo desafios - Fundamentos da Linguagem de Programação Java 3 de 3 - Multiplicação Simples/src/Program.java
@@ -0,0 +1,39 @@
+/*import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double A, B, media;
+
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+
+ //TODO: Complete os espaços em branco com uma possível solução para o desafio 
+        media = (     * 3.5 +     * 7.5)/11; 
+
+        System.out.printf("MEDIA = %.5f",     );
+        System.out.println();
+    }
+}  */
+
+//Abaixo o codigo ja resolvido por mim
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double A, B, media;
+
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+
+ //TODO: Complete os espaços em branco com uma possível solução para o desafio 
+        media = (A * 3.5 + B* 7.5)/11; 
+
+        System.out.printf("MEDIA = %.5f", media);
+        System.out.println();
+    }
+}
